3ea0564c8e3d28de028b3bfc41621b993a17dc96
jakewimmer <283714808+jakewimmer@users.noreply.github.com>
2026-09-11 03:57:19 -0700

    Import anyhow::Context in TLS fixture (Windows compile fix)

--- spotter-svc/src/tls_test_fixture.rs
+++ spotter-svc/src/tls_test_fixture.rs
@@ -14,6 +14,7 @@ use std::sync::Arc;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::time::Duration;
 
+use anyhow::Context as _;
 use native_tls::Identity;
 use rcgen::{BasicConstraints, CertificateParams, IsCa, Issuer, KeyPair};
 use secrecy::SecretString;
